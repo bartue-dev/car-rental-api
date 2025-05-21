88d60f41561b402a25f9de54b2bf5fc22a77576a
Bartue <[bartbatiancila28@gmail.com]>
2025-05-21 10:33:52 +0800

    Create a authValidation.js file. Then create a register validation to validation the register request

---
+++ validator/authValidator.js
@@ -0,0 +1,19 @@
+const { body, param } = require("express-validator");
+
+const isEmptyMsg = "must not be empty";
+const isLengthMsg = "must be atleast 4 characters or more";
+const passwordLength = "must be atleast 8 characters or more";
+const passwordContains = "must contains number, symbold and letters";
+
+const validateRegister = [
+  body("username").trim()
+    .notEmpty().withMessage(`Username ${isEmptyMsg}`)
+    .isLength({min: 4}).withMessage(`Username ${isLengthMsg}`),
+  body("password").trim()
+    .notEmpty().withMessage(`Password ${isEmptyMsg}`)
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])[A-Za-z\d#?!@$%^&*-]{8,}$/).withMessage(`Password ${passwordLength} and ${passwordContains}`)
+]
+
+module.exports = {
+  validateRegister
+}
