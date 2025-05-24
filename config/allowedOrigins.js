a210d57f3c988d78d4aa368a98d91beb7b4098c4
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-05-24 11:58:29 +0800

    Rename origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173", //react front end
   "http://localhost:3000",
   "http://localhost:3500",
-  "https://blog-api-0nvl.onrender.com" // production site name
+  "https://sitename.com" // production site name
 ]
 
 module.exports = allowedOrigins;
