48b8b34c765b72c005b3bc2e5b65ddeb219058c6
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-05-24 11:58:04 +0800

    Add comments

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const corsOptions = require("./config/corsOptions");
 const cookieParser = require("cookie-parser");
 const CustomErr = require("./utils/customErr");
 const {cronJobs} = require("./cronJobs")
-const router = require("./routes")
+const router = require("./routes");
+const verifyJwt  = require("./middleware/verifyJwt");
 
+//handles syncchronous error. Ex. undifined variable or function
+//it should shut down the app because the server already crashed
+//it should invoke before the app express() to make sure it work
 process.on("uncaughtException", (err) => {
   console.log(err.name, err.message);
   console.log("Uncaught Exception occured!, shutting down");
@@ -18,26 +22,42 @@ process.on("uncaughtException", (err) => {
 
 const app = express();
 
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
 app.use(credentials);
 
+// Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
+// middleware json
 app.use(express.json());
 
+// middleware for cookies
 app.use(cookieParser());
 
-//routes
+//auth routes
 app.use("/v1/register", router.registerRoute);
-app.use("/v1/login", router.loginRoute)
+app.use("/v1/login", router.loginRoute);
+app.use("v1/refreshToken", router.refreshTokenRoute);
+app.use("v1/logout", router.logoutRoute);
+
+
+//api routes
+app.use(verifyJwt);
+app.use("/v1/testimonials", router.testimonialsRoute);
+app.use("/v1/vehicle", router.vehicleRoute);
 
 //cron jobs
 cronJobs();
 
+//default route
+//handles error if the the url cannot find
 app.all(/(.*)/, (req, res, next) => {
   const err = new CustomErr(`Can't find ${req.originalUrl} on the server`, 404)
   next(err);
 });
 
+//errorHandler middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3500
@@ -45,6 +65,9 @@ const server = app.listen(PORT, () => {
   console.log(`Server listening to PORT ${PORT}`);
 });
 
+
+//handle rejection promise, if no catch block to a promise
+//it should shut down the app because the server already crashed
 process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
   console.log("Unhandled rejection occured!, shutting down");
@@ -54,3 +77,9 @@ process.on("unhandledRejection", (err) => {
   });
 });
 
+/* 
+  process.exit( code )
+  Code: It can be either 0 or 1. 
+  0 means end the process without any kind of failure and 
+  1 means end the process with some failure.
+*/
