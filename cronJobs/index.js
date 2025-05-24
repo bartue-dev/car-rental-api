de31875168e7effcef20bdf7e2f6c0a778eda45f
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-05-24 11:59:05 +0800

    Edit time. Cron job run every 2 days

--- cronJobs/index.js
+++ cronJobs/index.js
@@ -3,7 +3,7 @@ const { deleteExpiresToken } = require("./auth/refreshToken");
 
 const cronJobs = () => {
   // delete expires refresh token every 12 midnight
-  cron.schedule("* 24 * * *", deleteExpiresToken);
+  cron.schedule("* * */2 * *", deleteExpiresToken);
 
 };
 
