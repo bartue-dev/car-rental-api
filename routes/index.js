e9911229166e16e47aa5150fab78efd8ccc34ed8
Bartue <[bartbatiancila28@gmail.com]>
2025-05-24 12:05:09 +0800

    Import refreshTokenRoute, logoutRoute, testimonialsRoute and vehicleRoute

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 //auth
 const registerRoute = require("./auth/register");
 const loginRoute = require("./auth/login");
+const refreshTokenRoute = require("./auth/refreshToken");
+const logoutRoute = require("./auth/logout");
+
+
+//api
+const testimonialsRoute = require("./api/testimonials");
+const vehicleRoute = require("./api/vehicle");
 
 module.exports = {
   registerRoute,
   loginRoute,
+  refreshTokenRoute,
+  logoutRoute,
+  testimonialsRoute,
+  vehicleRoute
 }
