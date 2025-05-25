ea644c4bc9f18087a449efddb6d301c0d0f5addd
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-05-25 10:01:33 +0800

    transfer file to its repective roles

--- routes/api/testimonials.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const testimonialsCon = require("../../controllers/api/testimonialsController");
-
-router.post("/", testimonialsCon.createTestimonials);
-
-module.exports = router;
