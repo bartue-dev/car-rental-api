81d41715e82cfd24fb6af8a36a9324de04893414
Bartue <[bartbatiancila28@gmail.com]>
2025-05-25 10:01:33 +0800

    transfer file to its repective roles

--- routes/api/vehicle.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const vehicleCon = require("../../controllers/api/vehicleController")
-
-router.get("/", vehicleCon.addVehicle);
-
-module.exports = router;
