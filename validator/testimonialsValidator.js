fc6c3e28660c5af18ed9991770d4b760f06dd1e4
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-05-25 10:07:00 +0800

    Create an testimonialsValidator that validates all testimonails req

---
+++ validator/testimonialsValidator.js
@@ -0,0 +1,28 @@
+const {body, param} = require("express-validator");
+
+const isEmptyMsg = "must not be empty";
+
+const validateCreateTesmonials = [
+  body("content").trim()
+    .notEmpty().withMessage(`Content ${isEmptyMsg}`)
+]
+
+const validateDeleteTestimonial = [
+  param("testimonialId").trim()
+    .exists().withMessage("testimonailId not exist")
+    .isUUID().withMessage("testimonialId must be a valid UUID")
+]
+
+const validateUpdateTestimonial = [
+  body("content").trim()
+    .notEmpty().withMessage(`Content ${isEmptyMsg}`),
+  param("testimonialId").trim()
+    .exists().withMessage("testimonailId not exist")
+    .isUUID().withMessage("testimonialId must be a valid UUID")
+]
+
+module.exports = {
+  validateCreateTesmonials,
+  validateDeleteTestimonial,
+  validateUpdateTestimonial
+}
