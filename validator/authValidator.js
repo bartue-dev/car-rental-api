0894ad86a1ad4e04efd35435d46d522793d81d27
Bartue <[bartbatiancila28@gmail.com]>
2025-05-25 10:03:38 +0800

    Add a role validation to validateRegister. Only ADMIN and USER should be the valid value if not it should return an error message

--- validator/authValidator.js
+++ validator/authValidator.js
@@ -4,14 +4,27 @@ const isEmptyMsg = "must not be empty";
 const isLengthMsg = "must be atleast 4 characters or more";
 const passwordLength = "must be atleast 8 characters or more";
 const passwordContains = "must contains number, symbold and letters";
+const role = ["USER", "ADMIN"];
 
 const validateRegister = [
   body("username").trim()
     .notEmpty().withMessage(`Username ${isEmptyMsg}`)
-    .isLength({min: 4}).withMessage(`Username ${isLengthMsg}`),
+    .isLength({min: 4}).withMessage(`Username ${isLengthMsg}`)
+    .exists().withMessage("Username already exist"),
   body("password").trim()
     .notEmpty().withMessage(`Password ${isEmptyMsg}`)
-    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])[A-Za-z\d#?!@$%^&*-]{8,}$/).withMessage(`Password ${passwordLength} and ${passwordContains}`)
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])[A-Za-z\d#?!@$%^&*-]{8,}$/).withMessage(`Password ${passwordLength} and ${passwordContains}`),
+  body("role").trim()
+    .isUppercase().withMessage("role must be uppercase")
+    .custom(async value => {
+      const user = role.includes(value)
+      console.log(value)
+      if (user){
+        return true
+      } else {
+        throw new Error()
+      }
+    }).withMessage("role must be either USER or ADMIN")
 ]
 
 const validateLogin = [
