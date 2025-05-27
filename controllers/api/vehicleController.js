6ca7a99366a0b78f45afcb4e6b05b045cd07c842
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-05-27 21:30:40 +0800

    Create a addVehicle controller with valiadtion.

--- controllers/api/vehicleController.js
+++ controllers/api/vehicleController.js
@@ -1,10 +1,52 @@
 const asyncHandler = require("express-async-handler");
+const { vehicleMethods} = require("../../db/api/vehicleQueries");
+const { validationResult } = require("express-validator");
+const { validateAddVehicle } = require("../../validator/vehicleValidator")
+const CustomErr = require("../../utils/customErr");
+
+//admin
+exports.addVehicle = [validateAddVehicle, asyncHandler(async (req, res, next) => {
+  let { name, type, status, price } = req.body;
+  const { id } = req.user;
+
+  if (!id) {
+    const err = new CustomErr("Unauthorized", 401);
+    next(err);
+    return;
+  }
+
+  if (!files) {
+    const err = new CustomErr("Invalid file", 400);
+    next(err);
+    return
+  }
+
+  // validation
+  const validationErr = validationResult(req);
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Adding a vehicle failed",
+      errors: validationErr.array()
+    });
+  }
+
+  // save vehicle details to db
+  const vehicleDetails = await vehicleMethods.addVehicle(
+      name,
+      type,
+      status,
+      Number(price),
+      id
+  );
 
 
-exports.addVehicle = asyncHandler(async (req, res, next) => {
-  
+res.status(200).json({
+  status: "success",
+  message: "Vehicle added successfully",
+  data: {
+    vehicleDetails
+  }
+});
 
-  res.status(200).json({
-    message: "TESTING!"
-  })
-})
+})];
