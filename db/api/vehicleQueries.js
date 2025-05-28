1d56976ece6b73aa72613adffc27987e37e32d3c
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-05-28 21:57:06 +0800

    Add getAllVehicle, getVehicle, updateVehicle and deleteVehicle queries

--- db/api/vehicleQueries.js
+++ db/api/vehicleQueries.js
@@ -15,6 +15,47 @@ class Vehicle {
       }
     })
   }
+
+  async getAllVehicle() {
+    return await prisma.vehicle.findMany({
+      include: {
+        images: true
+      }
+    });
+  }
+
+  async getVehicle(vehicleId) {
+    return await prisma.vehicle.findUnique({
+      where: {
+        vehicleId: vehicleId
+      },
+      include: {
+        images: true
+      }
+    });
+  }
+
+  async updateVehicle(vehicleId, name, type, status, price) {
+    await prisma.vehicle.update({
+      where: {
+        vehicleId: vehicleId
+      },
+      data: {
+        name: name,
+        type: type,
+        status: status,
+        price: price
+      }
+    });
+  }
+
+  async deleteVehicle(vehicleId) {
+    await prisma.vehicle.delete({
+      where: {
+        vehicleId
+      }
+    });
+  }
 }
 
 const vehicleMethods = new Vehicle();
