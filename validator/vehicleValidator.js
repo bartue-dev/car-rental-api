42f13105979b9e4af6860a4cdd3f4cae5fc99c83
Bartue <[bartbatiancila28@gmail.com]>
2025-05-28 21:58:40 +0800

    Add validateGetVehicle, validateUpdaVehicle and validateDeleteVehicle. ALso added param from express validator

--- validator/vehicleValidator.js
+++ validator/vehicleValidator.js
@@ -1,4 +1,4 @@
-const {body} = require("express-validator");
+const {body, param} = require("express-validator");
 
 const isEmptyMsg = "must not be empty";
 
@@ -21,6 +21,43 @@ const validateAddVehicle = [
   }).withMessage(`Vehicle status must be iether Available or Not Available`),
 ]
 
+const validateGetVehicle = [
+  param("vehicleId")
+    .exists().withMessage("VehicleId not exist")
+    .isUUID().withMessage("VehicleId is not valid UUID")
+]
+
+const validateUpdateVehicle = [
+  param("vehicleId")
+    .exists().withMessage("VehicleId not exist")
+    .isUUID().withMessage("VehicleId is not valid UUID"),
+  body("name").trim()
+    .notEmpty().withMessage(`Vehicle name ${isEmptyMsg}`)
+    .isLength({ min: 4}).withMessage(`Name must be atleast 4 or more characters long`),
+  body("type").trim()
+    .notEmpty().withMessage(`Vehicle type ${isEmptyMsg}`),
+  body("price").trim()
+    .notEmpty().withMessage(`Vehicle price ${isEmptyMsg}`),
+  body("status").trim()
+  .notEmpty().withMessage(`Vehicle status ${isEmptyMsg}`)
+  .custom(value => {
+    if (value === "Available" || value === "Not Available") {
+      return true
+    } else {
+      throw new Error();
+    }
+  }).withMessage(`Vehicle status must be iether Available or Not Available`),
+  ]
+
+const validateDeleteVehicle = [
+  param("vehicleId")
+    .exists().withMessage("VehicleId not exist")
+    .isUUID().withMessage("VehicleId is not valid UUID"),
+]
+
 module.exports = {
   validateAddVehicle,
+  validateGetVehicle,
+  validateUpdateVehicle,
+  validateDeleteVehicle
 }
