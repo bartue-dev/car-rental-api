3984d5c71bdc323f3e99b3b63b968202c7a4ff40
Bartue <[bartbatiancila28@gmail.com]>
2025-05-28 21:54:56 +0800

    Remove files if statement. Rename json response message into Validation Error from the validation error

--- controllers/api/vehicleController.js
+++ controllers/api/vehicleController.js
@@ -1,10 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const { vehicleMethods} = require("../../db/api/vehicleQueries");
 const { validationResult } = require("express-validator");
-const { validateAddVehicle } = require("../../validator/vehicleValidator")
+const { validateAddVehicle, validateGetVehicle, validateUpdateVehicle, validateDeleteVehicle } = require("../../validator/vehicleValidator")
 const CustomErr = require("../../utils/customErr");
 
 //admin
+//add vehicle details
 exports.addVehicle = [validateAddVehicle, asyncHandler(async (req, res, next) => {
   let { name, type, status, price } = req.body;
   const { id } = req.user;
@@ -15,18 +16,12 @@ exports.addVehicle = [validateAddVehicle, asyncHandler(async (req, res, next) =>
     return;
   }
 
-  if (!files) {
-    const err = new CustomErr("Invalid file", 400);
-    next(err);
-    return
-  }
-
   // validation
   const validationErr = validationResult(req);
   if (!validationErr.isEmpty()) {
     return res.status(400).json({
       status: "Failed",
-      message: "Adding a vehicle failed",
+      message: "Validation Error",
       errors: validationErr.array()
     });
   }
@@ -50,3 +45,86 @@ res.status(200).json({
 });
 
 })];
+
+//retrieve all vehicle data
+exports.getAllVehicle = asyncHandler(async (req, res, next) => {
+  const allVehicle = await vehicleMethods.getAllVehicle();
+
+  res.status(200).json({
+    status: "success",
+    message: "Vehicle details retreive successfully",
+    data: {
+      allVehicle
+    }
+  })
+});
+
+//retrieve specific vehicle data
+exports.getVehicle = [validateGetVehicle, asyncHandler(async (req, res, next) => {
+  const {vehicleId} = req.params;
+  const validationErr = validationResult(req);
+
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Validation Error",
+      errors: validationErr.array()
+    })
+  }
+
+  const vehicleDetails = await vehicleMethods.getVehicle(vehicleId);
+
+  res.status(200).json({
+    status: "success",
+    message: "Vehicle data retrieve successfully",
+    data: {
+      vehicleDetails
+    }
+  });
+})];
+
+//update vehicle data
+exports.updateVehicle = [validateUpdateVehicle, asyncHandler(async (req, res, next) => {
+  const { name, type, status, price } = req.body;
+  const { vehicleId } = req.params;
+  const validationErr = validationResult(req);
+
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Validation Error",
+      errors: validationErr.array()
+    })
+  }
+
+  await vehicleMethods.updateVehicle(vehicleId, name, type, status, Number(price));
+
+  res.status(200).json({
+    status: "Success",
+    message: "Vehicle data updated successfully"
+  })
+})];
+
+//delete vehicle data
+exports.deleteVehicle = [validateDeleteVehicle, asyncHandler(async (req, res, next) => {
+  const { vehicleId } = req.params;
+  const validationErr = validationResult(req);
+
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Validation Error",
+      errors: validationErr.array()
+    });
+  }
+
+  await vehicleMethods.deleteVehicle(vehicleId);
+
+  res.status(204).json({
+    status: "success",
+    message: "Vehicle data deleted successfully",
+  })
+})];
+
+
+
