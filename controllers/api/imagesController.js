fa0d81d66a79fde7cc8a14f767527f264d288453
Bartue <[bartbatiancila28@gmail.com]>
2025-05-29 21:14:20 +0800

    Create a getImagesByVehicle, deleteImagesByVehicle, deleteImage controller

--- controllers/api/imagesController.js
+++ controllers/api/imagesController.js
@@ -3,9 +3,10 @@ const supabase = require("../../supabase/supabase");
 const { decode } = require("base64-arraybuffer");
 const { imagesMethods } = require("../../db/api/imagesQueries");
 const {validationResult} = require("express-validator");
-const {validateAddImages} = require("../../validator/imagesValidator")
+const {validateAddImages, validateDeleteImage, validateVehicleId} = require("../../validator/imagesValidator")
 const CustomErr = require("../../utils/customErr");
 
+//add images
 exports.addImages = [validateAddImages, asyncHandler(async (req, res, next) => {
   const files = req.files;
   const { id } = req.user;
@@ -65,4 +66,114 @@ exports.addImages = [validateAddImages, asyncHandler(async (req, res, next) => {
     }
   });
 
+})];
+
+//get all images by vehicle
+exports.getImagesByVehicle = [validateVehicleId, asyncHandler(async (req, res, next) => {
+  const { vehicleId } = req.params;
+  const validationErr = validationResult(req);
+
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Validation Error",
+      errors: validationErr.array()
+    });
+  }
+
+  const images = await imagesMethods.getAllImages(vehicleId);
+
+  if (!images) {
+    const err = new CustomErr("Cannot retrieve images", 400);
+    next(err);
+    return
+  }
+
+  res.status(200).json({
+    status: "Success",
+    message: "Images retrieve successfully",
+    data: {
+      images
+    }
+  })
+
+})];
+
+//delete images by vehicle
+exports.deleteImagesByVehicle = [validateVehicleId, asyncHandler(async (req, res, next) => {
+  const {vehicleId} = req.params;
+  const validationErr = validationResult(req);
+
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Validation Error",
+      errors: validationErr.array()
+    });
+  }
+
+  const imagesByVehicle = await imagesMethods.getImagesByVehicle(vehicleId);
+
+  for (const image of imagesByVehicle) {
+    
+    const url = image.url.split("/public/images/");
+
+    const filePath = url[1].replace(/%20/g, " ");
+
+     //delete image in supabase storage
+    const {data, error} = await supabase.storage
+      .from("images")
+      .remove([filePath]);
+
+    if (error) {
+      return res.status(400).json({
+        status: "Failed",
+        message: "Supabase error",
+        errors: error
+      });
+    }
+  }
+
+  await imagesMethods.deleteImagesByVehicle(vehicleId);
+
+  res.sendStatus(204)
+})];
+
+//delete specific image
+exports.deleteImage = [validateDeleteImage, asyncHandler(async (req, res, next) => {
+  const { imageId } = req.params;
+  const validationErr = validationResult(req);
+
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Validation Error",
+      errors: validationErr.array()
+    })
+  }
+
+  const image = await imagesMethods.getImage(imageId);
+
+  console.log("Image:", image)
+
+  const url = image.url.split("/public/images/");
+
+  const filePath = url[1].replace(/%20/g, " ");
+
+  //delete image in supabase storage
+  const {data, error} = await supabase.storage
+    .from("images")
+    .remove([filePath])
+
+  if (error) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Supabase error",
+      error: error
+    });
+  }
+
+  await imagesMethods.deleteImage(imageId);
+
+  res.sendStatus(204)
 })];
