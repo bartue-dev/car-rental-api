517e0d4b92f812981b0edffbdb1885d7f1651f10
Bartue <[bartbatiancila28@gmail.com]>
2025-06-01 12:27:56 +0800

    Change time from midnight to 11 pm

--- cronJobs/index.js
+++ cronJobs/index.js
@@ -1,11 +1,12 @@
 const cron = require("node-cron");
 const { deleteExpiresToken } = require("./auth/refreshToken");
 
-const flag = true;
+let flag = true;
+
 
 const cronJobs = () => {
-
-  cron.schedule("0 0 * * *", () => {
+  
+  cron.schedule("* 23 * * *", () => {
     // if else statement have a logic of if cronJob execute within the set time check the flag first
     // flag is true execute the deleteExpiresToken function otherwise change the value of flag to true
     // this means that the deleteExpiresToken function only run every other day
