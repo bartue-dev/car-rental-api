620f9d83d652f35168cf83571fb16804510afb8a
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-01 12:34:31 +0800

    Create validation for bookings controllers

---
+++ validator/bookingValidator.js
@@ -0,0 +1,36 @@
+const {body, param} = require("express-validator");
+
+const isEmptyMsg = "must not be empty";
+const isAlpha = "must be characters only"
+const isAlphaNum = "must contains character and numbers only"
+const isNum = "must be numbers only"
+
+const validationAddBooking = [
+  body("firstName").trim()
+    .notEmpty().withMessage(`First name ${isEmptyMsg}`),
+  body("lastName").trim()
+    .notEmpty().withMessage(`Last name ${isEmptyMsg}`)
+    .isAlpha().withMessage(`Last name ${isAlpha}`),
+  body("address").trim()
+    .notEmpty().withMessage(`Address ${isEmptyMsg}`),
+  body("phoneNumber").trim()
+    .notEmpty().withMessage(`Phone number ${isEmptyMsg}`)
+    .isMobilePhone("en-PH").withMessage("Invalid mobile number"),
+  body("pickupDateTime").trim()
+    .notEmpty().withMessage(`Pickup time ${isEmptyMsg}`),
+  body("returnDateTime").trim()
+    .notEmpty().withMessage(`Return time ${isEmptyMsg}`),
+  body("duration").trim()
+    .notEmpty().withMessage(`Duration ${isEmptyMsg}`)
+]
+
+const validateBookingId = [
+  param("bookingId").trim()
+    .exists().withMessage("bookingId doesn't exist")
+    .isUUID().withMessage("bookingId is not a valid UUID")
+]
+
+module.exports = {
+  validationAddBooking,
+  validateBookingId
+}
