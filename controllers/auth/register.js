5d0e0a386b0079d08aa7ca07e52baec1005efc0c
Bartue <[bartbatiancila28@gmail.com]>
2025-06-13 22:02:03 +0800

    Add a registerAdmin and registerUser controller. Where user doesnt need to manually input the role. WHile the admin should input the role manually

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,17 +1,18 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-const { validateRegister } = require("../../validator/authValidator");
+const { validateRegisterAdmin, validateRegisterUser } = require("../../validator/authValidator");
 const { validationResult } = require("express-validator")
 const { accountMethods } = require("../../db/authQueries");
 const CustomErr = require("../../utils/customErr");
 
-exports.register = [validateRegister ,asyncHandler(async (req, res, next) => {
+exports.registerAdmin = [validateRegisterAdmin ,asyncHandler(async (req, res, next) => {
   let { username, password, role } = req.body;
   const validationErr = validationResult(req);
 
   //validation
   if (!validationErr.isEmpty()) {
     return res.status(400).json({
+      status: "Failed",
       message: "Registration failed",
       errors: validationErr.array()
     });
@@ -20,6 +21,37 @@ exports.register = [validateRegister ,asyncHandler(async (req, res, next) => {
   const hashPassword = await bcrypt.hash(password, 10);
 
   const createdAccount = await accountMethods.createAccount(username, hashPassword, role);
+
+  if (!createdAccount) {
+    const err = new CustomErr(`Cannot create an account`, 400);
+    next(err)
+    return;
+  }
+
+  res.status(201).json({
+    status: "success",
+    message: "Account registered successfully",
+    account: createdAccount
+  })
+})];
+
+
+exports.registerUser = [validateRegisterUser ,asyncHandler(async (req, res, next) => {
+  let { username, password } = req.body;
+  const validationErr = validationResult(req);
+
+  //validation
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Registration failed",
+      errors: validationErr.array()
+    });
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const createdAccount = await accountMethods.createAccount(username, hashPassword);
 
   if (!createdAccount) {
     const err = new CustomErr(`Cannot create an account`, 400);
