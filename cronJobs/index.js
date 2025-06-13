400b9e7bb3db140c66e928728eae5ca538367f9b
Bartue <[bartbatiancila28@gmail.com]>
2025-06-13 22:03:24 +0800

    Edit the cronJobs time. The previous implementation was wrong instead of * 23 * * * this does nothing, it should be 0 23 * * * to run in evry 11pm of the night

--- cronJobs/index.js
+++ cronJobs/index.js
@@ -6,7 +6,7 @@ let flag = true;
 
 const cronJobs = () => {
   
-  cron.schedule("* 23 * * *", () => {
+  cron.schedule(" 0 23 * * *", () => {
     // if else statement have a logic of if cronJob execute within the set time check the flag first
     // flag is true execute the deleteExpiresToken function otherwise change the value of flag to true
     // this means that the deleteExpiresToken function only run every other day
