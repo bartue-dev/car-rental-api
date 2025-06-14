ebad29e2d6b3ee0fcf351e35601e32268d441687
Bartue <[bartbatiancila28@gmail.com]>
2025-06-14 11:50:02 +0800

    Deleted

--- routes/auth/register.js
+++
@@ -1,7 +0,0 @@
-const { Router, application } = require("express")
-const router = Router();
-const registerCon = require("../../controllers/auth/register");
-
-router.post("/", registerCon.register);
-
-module.exports = router;
