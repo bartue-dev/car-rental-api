23b37d9898b1abfe624679396608597bdf5c4949
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-06-14 11:55:15 +0800

    Add imagesAdmin route to admin folder

---
+++ routes/api/admin/imagesAdmin.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const router = Router();
+const imagesCon = require("../../../controllers/api/imagesController");
+const {authByRoleAdmin} = require("../../../middleware/authByRole");
+const multer = require("multer");
+
+router.use(authByRoleAdmin);
+
+const storage = multer.memoryStorage();
+
+const upload = multer({storage: storage});
+
+router.route("/vehicle/:vehicleId")
+  .post(upload.array("file"), imagesCon.addImages)
+  .get(imagesCon.getImagesByVehicle)
+  .delete(imagesCon.deleteImagesByVehicle)
+
+router.delete("/:imageId", imagesCon.deleteImage);
+
+module.exports = router;
