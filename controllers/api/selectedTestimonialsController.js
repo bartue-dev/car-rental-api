c0940dc0bdc60a598cbf8dcf76a1869f94d9cb6e
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-14 17:52:51 +0800

    Creates a selectedTestimonailsController that handles the selectedTestimonails logic for api request. Right now it have a get and post logic

---
+++ controllers/api/selectedTestimonialsController.js
@@ -0,0 +1,45 @@
+const asyncHandler = require("express-async-handler");
+const { selectedTestimonialsMethods } = require("../../db/api/selectedTestmonialQueries")
+const { validationResult } = require("express-validator")
+const { validateSelectedTestimonial } = require("../../validator/selectedTestimonialsValidator");
+const CustomErr = require("../../utils/customErr");
+
+//admin
+exports.selectTestimonial = [validateSelectedTestimonial ,asyncHandler(async (req, res, next) => {
+  const {testimonialId} = req.params;
+  const validationErr = validationResult(req);
+
+  if (!validationErr.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Validation Error",
+      errors: validationErr.array()
+    })
+  }
+
+  const selectedTestimonial = await selectedTestimonialsMethods.selectTestimonial(testimonialId);
+
+  if (!selectedTestimonial) {
+    const err = new CustomErr("Cannot select the selected testimonial", 400)
+    next(err);
+    return
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "testimonial selelected"
+  })
+})]
+
+//public
+exports.getSelectedTestimonials = asyncHandler(async (req, res, next) => {
+  const selectedTestimonials = await selectedTestimonialsMethods.getSelectedTestimonials();
+
+  res.status(200).json({
+    status: "success",
+    message: "selected testiomnials retrieve successfully",
+    data: {
+      selectedTestimonials
+    }
+  })
+})
